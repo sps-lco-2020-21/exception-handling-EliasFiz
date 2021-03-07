2e6d29d25e8a89930b29bb8374a40bc0f11baff4
Elias Fizesan <eliasfizesan@yahoo.com>
2021-03-07 12:03:39 +0100

    Triangle Exception
    Addition of triangle exception and triangle class

---
+++ ExceptionsExercises/ExceptionsExercises/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoubleStr("256");
+            ToDoubleStr("pi");
+            ToDoubleStr("1.23e456");
+            Console.ReadKey();
+        }
+
+        static void ToDoubleStr(string str)
+        {
+            double num = 0;
+            try
+            {
+                num = Convert.ToDouble(str);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("FormatException caught: {0}", e);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("OverflowException caught: {0}", e);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("InvalidCastException caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ ExceptionsExercises/ExceptionsExercises/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsExercises
+{
+    class Triangle
+    {
+        int _sideA, _sideB, _sideC;
+
+        List<int> _sides = new List<int>();
+        public Triangle(int a, int b, int c)
+        {
+            _sideA = a;
+            _sideB = b;
+            _sideC = c;
+        }
+
+        public bool IsRightAngled()
+        {
+            return (Math.Pow(_sideA, 2) + Math.Pow(_sideB, 2) == Math.Pow(_sideC, 2));
+        }
+
+        public void TriangleException(int length)
+        {
+            throw new ArgumentException(String.Format("The triangle is not valid because the side with length {0} is greater than the sum of the other two lengths", length),
+                                      "side");
+        }
+        public void isValid()
+        {
+            if (_sideA + _sideB > _sideC && _sideA + _sideC > _sideB && _sideB + _sideC > _sideA)
+                TriangleException(Math.Max(Math.Max(_sideA, _sideB), _sideC));
+        }
+    }
+}
